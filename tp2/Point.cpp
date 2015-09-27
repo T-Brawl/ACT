2de034eaf3661d63fd40f7b9dee7ab4ad67c3fe1
ethan valtena <ethanvaltena@gmail.com>
2015-09-27 23:29:48 +0200

    question 5 faite, début de la question 6 en cours

---
+++ tp2/Point.cpp
@@ -0,0 +1,5 @@
+Point::Point():x(0),y(0){
+}
+
+Point::Point(coord, absc):x(coord),y(absc){
+}
---
+++ tp2/Point.h
@@ -0,0 +1,11 @@
+class Point{
+  private:
+  int x;
+  int y;
+
+  public:
+  Point();
+  Point(int coord, int absc);
+  int getX();
+  int getY();
+}
