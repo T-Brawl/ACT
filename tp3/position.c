a0a99d27d6bafd304d8d9159d59a85fd585ed4e2
T-Brawl <t-brawl@hotmail.fr>
2015-10-08 21:54:29 +0200

    New algorithm that seems to work

--- tp3/position.c
+++ tp3/position.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
 #define max(a,b) \
    ({ __typeof__ (a) _a = (a); \
@@ -11,69 +13,95 @@
        __typeof__ (b) _b = (b); \
      _a < _b ? _a : _b; })
 
-int ctoi(char c){
-  printf("%d--", c);
-  printf("%d\n", c-48);
-  return (int)c-48;
+struct case_position {
+    unsigned int check;
+    int val;
+};
+
+int position_naive(unsigned int m, unsigned int n, unsigned int i, unsigned int j){
+    int tmp,res;
+    unsigned int cpt,flag;
+    res = 0;
+    /*
+    * The integer flag is meant to guide the automata.
+    * The flag is 0 as long as the recursive calls of position return stricly positive values.
+    * When one recursive call return a 0 or a negative value, flag is set to 1,
+    * res is reset to 0 and will be affected only by recursive calls negative values.
+    *
+    */
+    flag = 0;
+    assert(i<=m);
+    assert(j<=n);
+    assert(m);
+    assert(n);
+    assert(i);
+    assert(j);
+    if(m == 1 && n == 1) return 0;
+
+    for(cpt=1; cpt<m;cpt++){
+
+        if(cpt<i){
+            tmp = position_naive(m-cpt, n, i-cpt,j);
+        }else{
+            tmp = position_naive(cpt, n,i,j);
+        }
+
+        if(tmp <= 0) {res = 0; flag = 1;}
+        if(flag) {
+            if(tmp <= 0) res = res?max(tmp,res):tmp;
+        } else {
+            res=res?max(tmp,res):tmp;
+        }
+
+    }
+
+    for(cpt=1; cpt<n;cpt++){
+
+        if(cpt<j){
+            tmp = position_naive(m, n-cpt, i,j-cpt);
+        }else{
+            tmp = position_naive(m, cpt,i,j);
+        }
+
+        if(tmp <= 0) {res = 0; flag = 1;}
+        if(flag) {
+            if(tmp <= 0) res = res?max(tmp,res):tmp;
+        } else {
+            res=res?max(tmp,res):tmp;
+        }
+    }
+    if(flag) res = 1-res; else res = -1-res;
+    return res;
 }
 
-int position(int m,int n, int i, int j) {
-  int cpt,tmpPos, tmpNeg;
-  tmpPos=0;
-  tmpNeg=-1;
-  if(m==1 && n==1){
+int position_dynamique(unsigned int m, unsigned int n, unsigned int i, unsigned int j){
+
+    int a,b,c,d;
+
+    struct case_position array[m+1][n+1][i+1][j+1];
+    struct case_position *cell = malloc(sizeof(struct case_position));
+    cell->check = 0;
+    cell->val = 0;
+     for(a=1;a<=m;a++)
+        for(b=1;b<=n;b++)
+            for(c=1;c<=i && i<=m;c++)
+                for(d=1;d<=j && j<=n;d++) {
+                    printf("%d\t%d\t%d\t%d\n",a,b,c,d);
+                    array[a][b][c][d] = *cell;
+                }
+
+    array[1][1][1][1].check = 1;
+
     return 0;
-  }
-  for(cpt=1; cpt<m-1;cpt++){
-    int tmp;
-    if(cpt<=i){
-      tmp = position(m-cpt, n, i-cpt,j);
-    }else{
-      tmp = position(cpt, n,i,j);
-    }
-    if(tmp>0){
-      tmpPos=max(tmpPos,(tmp+1));
-    }else{
-      if(tmpNeg>0){
-	tmpNeg=min(tmpNeg, -(tmp-1));
-      }else{
-	tmpNeg= -(tmp-1);
-      }
-    }
-  }
-  for(cpt=1; cpt<n-1;cpt++){
-    int tmp;
-    if(cpt<=j){
-      tmp = position(m, n-cpt, i,j-cpt);
-    }else{
-      tmp = position(m, cpt,i,j);
-    }
-    if(tmp>0){
-      tmpPos=max(tmpPos,(tmp+1));
-    }else{
-      if(tmpNeg>0){
-	tmpNeg=min(tmpNeg, -(tmp-1));
-      }else{
-	tmpNeg= -(tmp-1);
-      }
-    }
-  }
-  if(tmpNeg!=-1){
-    return tmpNeg;
-  }
-  return tmpPos;
 }
 
+int main (int argc, char *argv[]){
 
+    int res;
 
+    //res = position(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
+    res = position_naive(100,100,50,50);
+    printf("Résultat = %d\n",res);
 
-int main(int argc,char **args) {
-  int res = position(ctoi(args[1][0]),
-		     ctoi(args[2][0]),
-		     ctoi(args[3][0]),
-		     ctoi(args[4][0]));
-
-  printf("%d",res);
-
-  return 0;
+    return 0;
 }
