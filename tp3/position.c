67efbc1efbd4c301e75736b3f64c28e6dfb9f1d7
Moevi Alexandre <moevi@a11p6.fil.univ-lille1.fr>
2015-10-14 17:18:11 +0200

    Wrong algo?

--- tp3/position.c
+++ tp3/position.c
@@ -13,10 +13,15 @@
        __typeof__ (b) _b = (b); \
      _a < _b ? _a : _b; })
 
+
 struct case_position {
     unsigned int check;
     int val;
 };
+
+typedef struct case_position * case_p;
+
+static case_p ****res_d;
 
 int position_naive(unsigned int m, unsigned int n, unsigned int i, unsigned int j){
     int tmp,res;
@@ -30,39 +35,88 @@ int position_naive(unsigned int m, unsigned int n, unsigned int i, unsigned int 
     *
     */
     flag = 0;
-    assert(i<=m);
-    assert(j<=n);
-    assert(m);
-    assert(n);
-    assert(i);
-    assert(j);
     if(m == 1 && n == 1) return 0;
 
     for(cpt=1; cpt<m;cpt++){
-
         if(cpt<i){
             tmp = position_naive(m-cpt, n, i-cpt,j);
         }else{
             tmp = position_naive(cpt, n,i,j);
         }
-
         if(tmp <= 0) {res = 0; flag = 1;}
         if(flag) {
             if(tmp <= 0) res = res?max(tmp,res):tmp;
         } else {
             res=res?max(tmp,res):tmp;
         }
-
     }
 
     for(cpt=1; cpt<n;cpt++){
-
         if(cpt<j){
             tmp = position_naive(m, n-cpt, i,j-cpt);
         }else{
             tmp = position_naive(m, cpt,i,j);
         }
+        if(tmp <= 0) {res = 0; flag = 1;}
+        if(flag) {
+            if(tmp <= 0) res = res?max(tmp,res):tmp;
+        } else {
+            res=res?max(tmp,res):tmp;
+        }
+    }
+    if(flag) res = 1-res; else res = -1-res;
+    return res;
+}
 
+void add_cell(int d0,int d1,int d2,int d3, int val){
+  struct case_position *cell = malloc(sizeof(struct case_position));
+  cell->check = 1;
+  cell->val = val;
+  res_d[d0][d1][d2][d3] = cell;
+}
+
+int dyna_rec(unsigned int m, unsigned int n, unsigned int i, unsigned int j){
+  if (res_d[m][n][i][j]==NULL){
+    return rec_for_dyna(m, n,i,j);
+  }else{
+    return res_d[m][n][i][j]->val;
+  }
+}
+
+int rec_for_dyna(unsigned int m, unsigned int n, unsigned int i, unsigned int j){
+  int tmp,res;
+    unsigned int cpt,flag;
+    res = 0;
+    /*
+    * The integer flag is meant to guide the automata.
+    * The flag is 0 as long as the recursive calls of position return stricly positive values.
+    * When one recursive call return a 0 or a negative value, flag is set to 1,
+    * res is reset to 0 and will be affected only by recursive calls negative values.
+    *
+    */
+    flag = 0;
+    if(m == 1 && n == 1) return 0;
+
+    for(cpt=1; cpt<m;cpt++){
+        if(cpt<i){
+            tmp = dyna_rec(m-cpt, n, i-cpt,j);
+        }else{
+            tmp = dyna_rec(cpt, n,i,j);
+        }
+        if(tmp <= 0) {res = 0; flag = 1;}
+        if(flag) {
+            if(tmp <= 0) res = res?max(tmp,res):tmp;
+        } else {
+            res=res?max(tmp,res):tmp;
+        }
+    }
+
+    for(cpt=1; cpt<n;cpt++){
+        if(cpt<j){
+            tmp = dyna_rec(m, n-cpt, i,j-cpt);
+        }else{
+            tmp = dyna_rec(m, cpt,i,j);
+        }
         if(tmp <= 0) {res = 0; flag = 1;}
         if(flag) {
             if(tmp <= 0) res = res?max(tmp,res):tmp;
@@ -75,32 +129,35 @@ int position_naive(unsigned int m, unsigned int n, unsigned int i, unsigned int 
 }
 
 int position_dynamique(unsigned int m, unsigned int n, unsigned int i, unsigned int j){
+  int k,l,o;
+  res_d = (case_p****)malloc(sizeof(case_p***)*m);
+  for(k=0; k<m; k++)
+    {
+      res_d[k] = (case_p***)malloc(sizeof(case_p**)*n);
+      for(l=0; l<n; l++)
+	{
+	  res_d[k][l] = (case_p**)malloc(sizeof(case_p*)*i);
+	  for( o=0; o<i;o++)
+	    {
+	      res_d[k][l][o] = (case_p*)malloc(sizeof(case_p)*j);
+	    }
+	}
+    }
+  
+  add_cell(0,0,0,0,0);
 
-    int a,b,c,d;
-
-    struct case_position array[m+1][n+1][i+1][j+1];
-    struct case_position *cell = malloc(sizeof(struct case_position));
-    cell->check = 0;
-    cell->val = 0;
-     for(a=1;a<=m;a++)
-        for(b=1;b<=n;b++)
-            for(c=1;c<=i && i<=m;c++)
-                for(d=1;d<=j && j<=n;d++) {
-                    printf("%d\t%d\t%d\t%d\n",a,b,c,d);
-                    array[a][b][c][d] = *cell;
-                }
-
-    array[1][1][1][1].check = 1;
-
-    return 0;
+  dyna_rec(m-1,n-1,i-1,j-1);
+  //free nothing, it's program's end  :D
+  return 0;
 }
 
 int main (int argc, char *argv[]){
 
     int res;
 
-    //res = position(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
-    res = position_naive(100,100,50,50);
+    res = position_naive(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
+    //res = position_naive(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
+    //res = position_naive(100,100,50,50);
     printf("Résultat = %d\n",res);
 
     return 0;
